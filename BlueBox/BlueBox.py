f59d227e1b6c524ae9dc39c491301ebe9ee5bb48
PolyPenguinDev <calebhaftel@gmail.com>
2024-03-23 11:49:51 -0400

    Spelling

--- BlueBox/BlueBox.py
+++ BlueBox/BlueBox.py
@@ -1,8 +1,8 @@
 import click
 import conveyor
 @click.command()
-@click.option('-s', '--standalone', type=str, help="Executes conveyor file outside of a BlueBox intance")
-@click.option('-e', '--exec', type=str, help="Executes conveyor file inside of a BlueBox intance")
+@click.option('-s', '--standalone', type=str, help="Executes conveyor file outside of a BlueBox instance")
+@click.option('-e', '--exec', type=str, help="Executes conveyor file inside of a BlueBox instance")
 def main(standalone, exec):
     if standalone:
         cnv = conveyor.cnv()
