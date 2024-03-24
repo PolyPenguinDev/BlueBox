d2fb2304beeee7321915d6c5731ee8df95d4373c
PolyPenguinDev <calebhaftel@gmail.com>
2024-03-24 18:17:31 -0400

    added file support

--- BlueBox/BlueBox.py
+++ BlueBox/BlueBox.py
@@ -3,6 +3,8 @@ import conveyor
 import os
 import json
 import random
+import pathlib
+
 @click.command()
 @click.option('-s', '--standalone', type=str, help="Executes conveyor file outside of a BlueBox instance")
 @click.option('-e', '--exec', type=str, help="Executes conveyor file inside of a BlueBox instance")
@@ -10,18 +12,19 @@ import random
 @click.option('-c', '--close', is_flag=True, help="Close the current BlueBox instance")
 
 def main(standalone, exec, instance, close):
+    inpath = str(pathlib.Path(__file__).parent.resolve())+'\\instances\\bbi.json'
     if instance:
-        with open('BlueBox/instances/bbi.json', 'w') as f:
+        with open(inpath, 'w') as f:
             json.dump({"requests":[], "session id":random.randrange(10000, 99999)}, f)
     elif close:
-        os.remove('BlueBox/instances/bbi.json')
+        os.remove(inpath)
     elif standalone:
         cnv = conveyor.cnv()
         cnv.execFile(standalone)
     elif exec:
         cnv = conveyor.cnv()
-        with open('BlueBox/instances/bbi.json', 'r') as f:
+        with open(inpath, 'r') as f:
             cnv.bb_sid = json.load(f)["session id"]
-        cnv.execFile(standalone)
+        cnv.execFile(exec)
 if __name__ == "__main__":
     main()
